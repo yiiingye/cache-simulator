45ed20516d429e94ed064903d4eab4231a93f0f6
yiiingye <qtyingye@gmail.com>
2026-03-18 15:22:34 +0100

    implemented 2 way set associative cache and added hit rate visualization with chartjs

--- cache.cpp
+++ cache.cpp
@@ -1,248 +1,278 @@
 #include <cstddef>
+#include <fstream>
 #include <iostream>
-#include <iterator>
+#include <ostream>
 #include <random>
-#include <utility>
 #include <vector>
-
-const int CACHE_LINES = 16;
-const int BLOCK_SIZE = 8;
-const unsigned long MEMORY_SIZE = 65536;
+constexpr size_t CACHE_LINES = 12;
+constexpr size_t BLOCK_SIZE = 8;
+constexpr size_t MEMORY_SIZE = 1024;
+constexpr size_t TWO_WAY_SET = 2;
 
 class Cache {
 public:
-  unsigned long hits;
-  unsigned long misses;
-  unsigned long accesses;
-  Cache() {
-    misses = 0;
-    hits = 0;
-    accesses = 0;
-  }
-  virtual void clean() {
-    misses = 0;
-    hits = 0;
-    accesses = 0;
-  }
-  virtual void printStats() { std::cout << "Statistics" << std::endl; };
-};
-class Line {
-public:
-  unsigned long tag;
-  bool valid;
+  unsigned long hits = 0;
+  unsigned long misses = 0;
+  unsigned long accesses = 0;
 
-  Line() : tag(0), valid(false) {}
+  virtual ~Cache() = default;
+  virtual void access(unsigned long address) = 0;
+  virtual void clean() { hits = misses = accesses = 0; }
+  virtual void printStats() const {
+    std::cout << "Accesses: " << accesses << "\n";
+    std::cout << "Hits: " << hits << "\n";
+    std::cout << "Misses: " << misses << "\n";
+
+    double hit_rate = accesses ? (double)hits / accesses * 100.0 : 0.0;
+    std::cout << "\033[31m"
+              << "Hit Rate: " << hit_rate << "%"
+              << "\033[0m\n";
+  }
 };
 
-// Direct mapping
+struct Line {
+  unsigned long tag = 0;
+  bool valid = false;
+};
+
 class CacheDM : public Cache {
 public:
   std::vector<Line> lines;
 
-  CacheDM() {
-    lines.resize(CACHE_LINES);
-    hits = 0;
-    misses = 0;
-    accesses = 0;
-  }
+public:
+  CacheDM() : lines(CACHE_LINES) {}
 
-  void clean() {
-    for (size_t i = 0; i < CACHE_LINES; i++) {
-      lines[i].tag = 0;
-      lines[i].valid = false;
+  void access(unsigned long address) override {
+    accesses++;
+
+    unsigned long block_addr = address / BLOCK_SIZE;
+    size_t index = block_addr % CACHE_LINES;
+    unsigned long tag = block_addr / CACHE_LINES;
+
+    Line &line = lines[index];
+
+    if (line.valid && line.tag == tag) {
+      hits++;
+    } else {
+      misses++;
+      line.valid = true;
+      line.tag = tag;
     }
   }
 
-  void access_direct(unsigned long address);
-  void printStats() override {
-    std::cout << "\nDirect Mapping Cache Statistics\n";
-    std::cout << "Accesses: " << accesses << "\n";
-    std::cout << "Hits: " << hits << "\n";
-    std::cout << "Misses: " << misses << "\n";
-
-    double hit_rate = accesses ? (double)hits / accesses * 100.0 : 0.0;
-    std::cout << "Hit Rate: " << hit_rate << "%\n";
-  };
-};
-
-void CacheDM::access_direct(unsigned long address) {
-  accesses++;
-
-  unsigned long block_addr = address / BLOCK_SIZE;
-  unsigned long index = block_addr % CACHE_LINES;
-  unsigned long tag = block_addr / CACHE_LINES;
-
-  Line &line = lines[index];
-
-  if (line.valid && line.tag == tag) {
-    hits++;
-  } else {
-    misses++;
-    line.valid = true;
-    line.tag = tag;
+  void clean() override {
+    Cache::clean();
+    for (auto &line : lines) {
+      line.valid = false;
+      line.tag = 0;
+    }
   }
-}
 
-// N-set associative
-class CacheNSetAssoc : public Cache {
-public:
-  void access_2_way_set();
-};
-
-void CacheNSetAssoc::access_2_way_set() {}
-
-class FullyLine {
-public:
-  bool valid;
-  unsigned long counter;
-  unsigned long tag;
-  FullyLine() {
-    tag = 0;
-    counter = 0;
-    valid = false;
+  void printStats() const override {
+    std::cout << "\n[Direct Mapped Cache]\n";
+    Cache::printStats();
   }
 };
 
-// fully associative
+struct FullyLine {
+  bool valid = false;
+  unsigned long tag = 0;
+  unsigned long age = 0;
+};
+
 class CacheFullyAssoc : public Cache {
 public:
   std::vector<FullyLine> lines;
-  int replace;
+  unsigned long replacements = 0;
 
-  CacheFullyAssoc() {
-    lines.resize(CACHE_LINES);
-    replace = 0;
+public:
+  CacheFullyAssoc() : lines(CACHE_LINES) {}
+
+  void access(unsigned long address) override {
+    accesses++;
+
+    unsigned long block_addr = address / BLOCK_SIZE;
+    unsigned long tag = block_addr;
+
+    for (auto &line : lines)
+      if (line.valid)
+        line.age++;
+
+    for (auto &line : lines) {
+      if (line.valid && line.tag == tag) {
+        hits++;
+        line.age = 0;
+        return;
+      }
+    }
+
+    misses++;
+
+    for (auto &line : lines) {
+      if (!line.valid) {
+        line.valid = true;
+        line.tag = tag;
+        line.age = 0;
+        return;
+      }
+    }
+
+    replacements++;
+    size_t lru_idx = 0;
+    for (size_t i = 1; i < lines.size(); i++)
+      if (lines[i].age > lines[lru_idx].age)
+        lru_idx = i;
+
+    lines[lru_idx].tag = tag;
+    lines[lru_idx].age = 0;
   }
-  void access_fully(unsigned long tag);
-  std::pair<bool, int> check_no_full();
-  bool in_cache(unsigned long tag);
-  std::pair<int, unsigned long> less_used();
+
   void clean() override {
-    misses = 0;
-    hits = 0;
-    accesses = 0;
-    replace = 0;
+    Cache::clean();
+    replacements = 0;
+    for (auto &line : lines) {
+      line.valid = false;
+      line.age = 0;
+      line.tag = 0;
+    }
   }
-  void printStats() override {
-    std::cout << "\nFully Associative Cache Statistics\n";
-    std::cout << "Accesses: " << accesses << "\n";
-    std::cout << "Hits: " << hits << "\n";
-    std::cout << "Misses: " << misses << "\n";
-    std::cout << "Replacements: " << replace << "\n";
-    double hit_rate = accesses ? (double)hits / accesses * 100.0 : 0.0;
-    std::cout << "Hit Rate: " << hit_rate << "%\n";
+
+  void printStats() const override {
+    std::cout << "\n[Fully Associative Cache]\n";
+    Cache::printStats();
+    std::cout << "Replacements: " << replacements << "\n";
   }
 };
 
-std::pair<bool, int> CacheFullyAssoc::check_no_full() {
-  for (auto it = lines.begin(); it != lines.end(); ++it) {
-    if (!it->valid) {
-      return {true, std::distance(lines.begin(), it)};
-    }
-  }
-  return {false, -1};
-}
-bool CacheFullyAssoc::in_cache(unsigned long tag) {
+struct TwoWayLine {
+  unsigned long tag = 0;
+  bool valid = 0;
+  unsigned long age = 0;
+};
 
-  for (auto &line : lines) {
-    if (line.tag == tag and line.valid) {
-      return true;
-    }
-  }
+class Cache2SetWayAssociative : public Cache {
+public:
+  std::vector<std::vector<TwoWayLine>> sets;
+  unsigned long replacements;
 
-  return false;
-}
-
-std::pair<int, unsigned long> CacheFullyAssoc::less_used() {
-
-  std::pair<int, unsigned long> min; 
-  min.first = -1;
-  min.second = lines[0].counter;
-  for (auto it = lines.begin() + 1; it < lines.end(); it++) {
-    if (it->counter < min.second) {
-      min.second = it->counter;
-      min.first = std::distance(lines.begin(), it);
-    }
+  Cache2SetWayAssociative() {
+    size_t set_size = CACHE_LINES / TWO_WAY_SET;
+    sets.resize(set_size, std::vector<TwoWayLine>(TWO_WAY_SET));
+    replacements = 0;
   }
 
-  return min;
-}
-
-void CacheFullyAssoc::access_fully(unsigned long address) {
-  accesses++;
-
-  unsigned long block_addr = address / BLOCK_SIZE;
-  unsigned long tag = block_addr;
-
-  for (auto &line : lines) {
-    if (line.valid)
-      line.counter++;
-  }
-
-  for (auto &line : lines) {
-    if (line.valid && line.tag == tag) {
-      hits++;
-      line.counter = 0;
-      return;
+  void clean() override {
+    Cache::clean();
+    replacements = 0;
+    for (auto &set : sets) {
+      for (auto &line : set) {
+        line.tag = 0;
+        line.valid = false;
+        line.age = 0;
+      }
     }
   }
 
-  misses++;
-  for (auto &line : lines) {
-    if (!line.valid) {
-      line.valid = true;
-      line.tag = tag;
-      line.counter = 0;
-      return;
+  void access(unsigned long address) override {
+    accesses++;
+
+    unsigned long block_addr = address / BLOCK_SIZE;
+    size_t set = block_addr % (CACHE_LINES / TWO_WAY_SET);
+    unsigned long tag = block_addr / (CACHE_LINES / TWO_WAY_SET);
+
+    for (auto &line : sets[set])
+      if (line.valid)
+        line.age++;
+
+    for (auto &line : sets[set]) {
+      if (line.valid && line.tag == tag) {
+        hits++;
+        line.age = 0;
+        return;
+      }
     }
+
+    misses++;
+
+    for (auto &line : sets[set]) {
+      if (!line.valid) {
+        line.valid = true;
+        line.tag = tag;
+        line.age = 0;
+        return;
+      }
+    }
+
+    replacements++;
+    size_t lru = 0;
+    for (size_t i = 1; i < sets[set].size(); i++)
+      if (sets[set][i].age > sets[set][lru].age)
+        lru = i;
+
+    sets[set][lru].tag = tag;
+    sets[set][lru].valid = true;
+    sets[set][lru].age = 0;
   }
 
-  int lru_idx = 0;
-  for (size_t  i = 1; i < lines.size(); i++) {
-    if (lines[i].counter > lines[lru_idx].counter) {
-      lru_idx = i;
-    }
+  void printStats() const override {
+    std::cout << "\n[2-Way Set Associative Cache]\n";
+    Cache::printStats();
+    std::cout << "Replacements: " << replacements << "\n";
   }
-
-  replace++;
-  lines[lru_idx].tag = tag;
-  lines[lru_idx].counter = 0;
-}
+};
 
 int main() {
-
   CacheDM cache_dm;
-  CacheFullyAssoc cache_fully_assoc;
-  std::random_device rd;
-  std::mt19937 rng(rd());
-  std::uniform_int_distribution<unsigned long> dist(0, MEMORY_SIZE - 1);
+  CacheFullyAssoc cache_fully;
+  Cache2SetWayAssociative cache_2_way_set;
 
+  std::ofstream myfile("cache_stats.csv");
+  myfile << "AccessType,CacheType,Hits,Misses,HitRate,Replacements\n";
+
+  std::mt19937 rng(std::random_device{}());
+  std::uniform_int_distribution<unsigned long> dist(0, MEMORY_SIZE - 1);
   const int NUM_ACCESSES = 200000;
 
-  std::cout << "\n\n+-------------+\n| RANDOM ADDR |\n+-------------+";
-
   for (int i = 0; i < NUM_ACCESSES; i++) {
-    unsigned long addr = dist(rng);
-    cache_dm.access_direct(addr);
-    cache_fully_assoc.access_fully(addr);
+    auto addr = dist(rng);
+    cache_dm.access(addr);
+    cache_fully.access(addr);
+    cache_2_way_set.access(addr);
   }
 
-  cache_dm.printStats();
-  cache_fully_assoc.printStats();
+  myfile << "Random,DirectMapping," << cache_dm.hits << "," << cache_dm.misses << ","
+         << (double)cache_dm.hits / cache_dm.accesses * 100 << ",0\n";
+  myfile << "Random,FullyAssociative," << cache_fully.hits << ","
+         << cache_fully.misses << ","
+         << (double)cache_fully.hits / cache_fully.accesses * 100 << ","
+         << cache_fully.replacements << "\n";
+  myfile << "Random,2WaySet," << cache_2_way_set.hits << ","
+         << cache_2_way_set.misses << ","
+         << (double)cache_2_way_set.hits / cache_2_way_set.accesses * 100 << ","
+         << cache_2_way_set.replacements << "\n";
+
   cache_dm.clean();
-  cache_fully_assoc.clean();
-
-  std::cout
-      << "\n\n+-----------------+\n| SEQUENTIAL ADDR |\n+-----------------+";
+  cache_fully.clean();
+  cache_2_way_set.clean();
 
   for (int i = 0; i < NUM_ACCESSES; i++) {
-    unsigned long addr = i % MEMORY_SIZE;
-    cache_dm.access_direct(addr);
-    cache_fully_assoc.access_fully(addr);
+    auto addr = i % MEMORY_SIZE;
+    cache_dm.access(addr);
+    cache_fully.access(addr);
+    cache_2_way_set.access(addr);
   }
 
-  cache_dm.printStats();
-  cache_fully_assoc.printStats();
+  myfile << "Sequential,DirectMapping," << cache_dm.hits << "," << cache_dm.misses
+         << "," << (double)cache_dm.hits / cache_dm.accesses * 100 << ",0\n";
+  myfile << "Sequential,FullyAssociative," << cache_fully.hits << ","
+         << cache_fully.misses << ","
+         << (double)cache_fully.hits / cache_fully.accesses * 100 << ","
+         << cache_fully.replacements << "\n";
+  myfile << "Sequential,2WaySet," << cache_2_way_set.hits << ","
+         << cache_2_way_set.misses << ","
+         << (double)cache_2_way_set.hits / cache_2_way_set.accesses * 100 << ","
+         << cache_2_way_set.replacements << "\n";
 
+  myfile.close();
   return 0;
 }
